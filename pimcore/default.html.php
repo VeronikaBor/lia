6450d384affbc91253b6750ddc9dab0d4ec7e17f
Veronika Borup <veronika.borup.syne17@tucsweden.se>
2018-05-07 09:56:11 +0200

    default/global/layout

---
+++ pimcore/default.html.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @var \Pimcore\Templating\PhpEngine $this
+ * @var \Pimcore\Templating\PhpEngine $view
+ * @var \Pimcore\Templating\GlobalVariables $app
+ */
+$this->extend('layout.html.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Ateles teamsite</title>
+</head>
+
+<body>
+<div class="header">
+  <div class="header_image" >
+    <?= $this->image("h_image") ?>
+  </div>
+  <div class="city_image">
+    <?= $this->image("city_image") ?>
+  </div>
+  
+</div>
+
+<div class="employee_grid">
+<!--?= $this->inc(3) ?-->
+</div>
+
+</body>
+</html>
---
+++ pimcore/layout.html.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @var \Pimcore\Templating\PhpEngine $this
+ * @var \Pimcore\Templating\PhpEngine $view
+ * @var \Pimcore\Templating\GlobalVariables $app
+ */ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ateles team site</title>
+    <link rel="stylesheet" type="text/css" href="/static/css/global.css" />
+</head>
+
+<body>
+
+    <div class="container">
+        <?php $this->slots()->output('_content') ?>
+    </div>
+</body>
+</html>
