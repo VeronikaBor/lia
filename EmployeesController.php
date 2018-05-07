247f6bf6239088f31fe9154a7862e814c2b0097e
Veronika Borup <veronika.borup.syne17@tucsweden.se>
2018-05-07 12:24:42 +0200

    controller

---
+++ EmployeesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Pimcore\Model\DataObject\Employees;
+use Symfony\Component\HttpFoundation\Request;
+use Zend\Paginator\Paginator;
+
+class EmployeesController extends FrontendController
+{
+    public function employeesAction(Request $request)
+    {
+	
+    }
+}
